ae62b3ebc744e6c550d72cdd0107417cf3476ae4
Williams Madu <williams.madu97@gmail.com>
2025-03-13 10:22:31 +0100

    Add cors package & its corresponding app.py config

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from controllers.expiry_controller import expiry_bp
 from utils.error_handler import error_handler
 from utils.logger import logger
 from config import PORT
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(expiry_bp, url_prefix="/api")
 error_handler(app)
